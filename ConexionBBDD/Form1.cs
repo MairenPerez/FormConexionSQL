515dd8ff0112f8523f413c5cb37998f557e4bbd3
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-11-05 10:09:49 +0100

    Método para actualizar textbox Mejora de código, añadir comentarios.

--- ConexionBBDD/Form1.cs
+++ ConexionBBDD/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ConexionBBDD
@@ -18,23 +12,38 @@ namespace ConexionBBDD
         public ConexionSQL()
         {
             InitializeComponent();
+            ActualizarEstadoBBDD();
         }
 
+        /// <summary>
+        /// Actualiza el textbox con el estado de la conexión a la BBDD
+        /// </summary>
+        private void ActualizarEstadoBBDD()
+        {
+            txtEstadoBBDD.Text = btnConectar.Enabled ? "Closed" : "Open";
+        }
+
+        /// <summary>
+        /// Llamamos al método ActualizarEstadoBBDD cada vez que se modifica el texto del textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void txtEstadoBBDD_TextChanged(object sender, EventArgs e)
         {
-            if (btnConectar.Enabled)
-                txtEstadoBBDD.Text = "Closed";
-            else
-                txtEstadoBBDD.Text = "Open";
+            ActualizarEstadoBBDD();
         }
 
+        /// <summary>
+        ///  Hacemos la conexión a la BBDD
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConectar_Click(object sender, EventArgs e)
         {
             string cadenaConexion = "Server=85.208.21.117,54321;Database=MairenEmployees;" +
-                "User Id=sa;" +
-                "Password=Sql#123456789;" +
-                "TrustServerCertificate=True;";
-
+                                    "User Id=sa;" +
+                                    "Password=Sql#123456789;" +
+                                    "TrustServerCertificate=True;";
 
             try
             {
@@ -43,7 +52,7 @@ namespace ConexionBBDD
                 MessageBox.Show("Conexión establecida con éxito");
                 btnConectar.Enabled = false;
                 btnDesconectar.Enabled = true;
-                txtEstadoBBDD.Text = "Open"; // Actualiza el estado del TextBox
+                ActualizarEstadoBBDD();
             }
             catch (Exception ex)
             {
@@ -51,17 +60,22 @@ namespace ConexionBBDD
             }
         }
 
+        /// <summary>
+        ///  Cerramos la conexión a la BBDD
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnDesconectar_Click(object sender, EventArgs e)
         {
             try
             {
-                if (conexion != null && conexion.State == ConnectionState.Open)
+                if (conexion?.State == ConnectionState.Open)
                 {
                     conexion.Close();
                     MessageBox.Show("Conexión cerrada con éxito");
                     btnConectar.Enabled = true;
                     btnDesconectar.Enabled = false;
-                    txtEstadoBBDD.Text = "Closed"; // Actualiza el estado del TextBox
+                    ActualizarEstadoBBDD();
                 }
                 else
                 {
