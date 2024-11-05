d3e09496849383ab9573852a9d925eefe5573fbd
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-11-05 09:40:08 +0100

    Agregar archivos de proyecto.

---
+++ ConexionBBDD/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ConexionBBDD
+{
+    partial class ConexionSQL
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEstadoBBDD = new System.Windows.Forms.TextBox();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEstadoBBDD
+            // 
+            this.txtEstadoBBDD.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtEstadoBBDD.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstadoBBDD.Location = new System.Drawing.Point(12, 12);
+            this.txtEstadoBBDD.Name = "txtEstadoBBDD";
+            this.txtEstadoBBDD.Size = new System.Drawing.Size(393, 27);
+            this.txtEstadoBBDD.TabIndex = 0;
+            this.txtEstadoBBDD.Text = "Closed";
+            this.txtEstadoBBDD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtEstadoBBDD.TextChanged += new System.EventHandler(this.txtEstadoBBDD_TextChanged);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesconectar.Location = new System.Drawing.Point(217, 58);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(188, 42);
+            this.btnDesconectar.TabIndex = 2;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(12, 58);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(182, 42);
+            this.btnConectar.TabIndex = 3;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            // 
+            // ConexionSQL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 414);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.txtEstadoBBDD);
+            this.Name = "ConexionSQL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conexión  a SQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEstadoBBDD;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ ConexionBBDD/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexionBBDD
+{
+    public partial class ConexionSQL : Form
+    {
+        public ConexionSQL()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Si el botón Conectar está habilitado, cambiamos el texto a "Conectado"
+        /// Si está deshabilitado, cambiamos el texto a "Desconectado"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtEstadoBBDD_TextChanged(object sender, EventArgs e)
+        {
+            if (btnConectar.Enabled == true)
+                txtEstadoBBDD.Text = "Conectado";
+            else 
+                txtEstadoBBDD.Text = "Desconectado";
+        }
+    }
+}
